488a00c26339743f2cd5ff9337336a5b408a1bff
Sai Prasanth <135234733+Prasanth011@users.noreply.github.com>
2026-08-25 22:15:37 +0530

    Update home route message for wedding invitation

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Prasanth!"
+    return "Meghana and Prasanth getting married on novemeber 26, 2026. All are invited!"
 
 app.run(host="0.0.0.0", port=5000)
